c874eea5d6db50a290121145f9236c9a681f8e67
Francois <francoismeiring0203@gmail.com>
2025-11-20 21:26:53 +0200

    Test Harness

---
+++ tests_harness/__init__.py
@@ -0,0 +1,6 @@
+"""Test harness package initializer.
+
+This file makes `tests_harness` importable during CI and local test runs.
+"""
+
+__all__ = ["generators", "unit"]
---
+++ tests_harness/generators/__init__.py
@@ -0,0 +1,5 @@
+"""Generators package for the test harness."""
+
+from .simulate import generate_delayed_sine, save_pair
+
+__all__ = ["generate_delayed_sine", "save_pair"]
