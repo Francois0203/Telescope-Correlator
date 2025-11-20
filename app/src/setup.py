2224276577a34b4e5b200ffa619bbada29c2b7d1
Francois <francoismeiring0203@gmail.com>
2025-11-20 21:24:44 +0200

    Create setup.py

---
+++ app/src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="telescope-correlator",
+    version="0.0.0",
+    description="Telescope correlator package",
+    packages=find_packages(),
+    include_package_data=True,
+)
